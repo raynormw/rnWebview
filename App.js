cd28246df22b06a798462d0eb2fe2d832b829fad
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2021-01-30 05:20:20 +0700

    :wrench: Loader done

--- App.js
+++ App.js
@@ -11,16 +11,36 @@ import {StyleSheet, View, ActivityIndicator} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: true,
+    };
+  }
+
+  hideSpinner() {
+    this.setState({visible: false});
+  }
+
   render() {
     return (
-      <WebView
-        style={styles.container}
-        source={{
-          uri:
-            'http://180.215.13.119/core/m/Default.aspx?ref=99bet303&mobile=auto',
-        }}>
-        {/* <ActivityIndicator size="large" color="#0000ff" /> */}
-      </WebView>
+      <View style={styles.container}>
+        <WebView
+          onLoad={() => this.hideSpinner()}
+          source={{
+            uri:
+              'http://180.215.13.119/core/m/Default.aspx?ref=99bet303&mobile=auto',
+          }}
+        />
+        {this.state.visible && (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            style={styles.loader}
+          />
+        )}
+      </View>
     );
   }
 }
@@ -29,6 +49,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  loader: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   },
 });
 
